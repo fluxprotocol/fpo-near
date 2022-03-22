5db33a9c421fc618bea47a716b7df3f22201b8e3
Jameson Hodge <contact@jamesonhodge.com>
2022-03-22 17:48:10 -0400

    feat: median from multiple price pairs ids; cleanup

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use near_sdk::{env, StorageUsage, Balance, AccountId, Promise};
+use near_sdk::json_types::U128;
 
 pub fn refund_storage(initial_storage: StorageUsage, sender_id: AccountId) {
     let current_storage = env::storage_usage();
@@ -18,5 +19,21 @@ pub fn refund_storage(initial_storage: StorageUsage, sender_id: AccountId) {
     };
     if refund_amount > 0 {
         Promise::new(sender_id).transfer(refund_amount);
+    }
+}
+
+pub fn mean(numbers: &Vec<u128>) -> U128 {
+    let sum: u128 = numbers.iter().sum();
+    U128::from( sum as u128 / numbers.len() as u128)
+
+}
+pub fn median(numbers: &mut Vec<u128>) -> U128 {
+    numbers.sort();
+
+    let mid = numbers.len() / 2;
+    if numbers.len() % 2 == 0 {
+        mean(&vec![numbers[mid - 1], numbers[mid]]) as U128
+    } else {
+        U128::from(numbers[mid])
     }
 }
