dac241889f79218ad8f861ef1fd0e5cf78e4ea3d
mnaga <mennat.abuelnaga@gmail.com>
2022-03-23 07:08:33 +0200

    chore: add scripts

--- scripts/deploy_fpo.sh
+++ scripts/deploy_fpo.sh
@@ -3,25 +3,24 @@
 # default params
 network=${network:-testnet}
 accountId=${accountId:-fpo.mennat0.testnet}
-oracle=${oracle:-07.oracle.flux-dev}
-paymentToken=${paymentToken:-v2.wnear.flux-dev}
+# oracle=${oracle:-07.oracle.flux-dev}
+# paymentToken=${paymentToken:-v2.wnear.flux-dev}
+master=${master:-mennat0.testnet}
+initialBalance=${initialBalance:-5}
 
-while [ $# -gt 0 ]; do
-
-   if [[ $1 == *"--"* ]]; then
-        param="${1/--/}"
-        declare $param="$2"
-        # echo $1 $2 // Optional to see the parameter:value result
-   fi
-
-  shift
-done
+# reset fpo account
+NEAR_ENV=$network near delete $accountId $master
+NEAR_ENV=$network near create-account $accountId --masterAccount $master --initialBalance $initialBalance
 
 # Register account with wNEAR contract and Oracle contract, give 1 NEAR to store with oracle to allow for multiple Data Requests to be made
-near call $paymentToken storage_deposit "{\"account_id\": \"$accountId\"}" --accountId $accountId --amount 0.00125 --gas=300000000000000
-near call $oracle storage_deposit "{\"account_id\": \"$accountId\"}" --accountId $accountId --amount 1 --gas=300000000000000
+# near call $paymentToken storage_deposit "{\"account_id\": \"$accountId\"}" --accountId $accountId --amount 0.00125 --gas=300000000000000
+# near call $oracle storage_deposit "{\"account_id\": \"$accountId\"}" --accountId $accountId --amount 1 --gas=300000000000000
 
 # Deposit 2 NEAR to get 2 wNEAR tokens to use in your contract
-near call $paymentToken near_deposit "{}" --accountId $accountId --amount 2 --gas=300000000000000
+# near call $paymentToken near_deposit "{}" --accountId $accountId --amount 2 --gas=300000000000000
 
+#deploy fpo
 NEAR_ENV=$network near deploy --accountId $accountId --wasmFile target/wasm32-unknown-unknown/release/near_fpo.wasm
+
+#initialize fpo
+NEAR_ENV=$network near call $accountId new --accountId $master --gas=300000000000000
---
+++ scripts/get_entry.sh
@@ -0,0 +1,15 @@
+
+accountId=${accountId:-requester.mennat0.testnet} 
+
+masterAccountId=${masterAccountId:-mennat0.testnet}
+initialBalance=${initialBalance:-5}
+fpoAccountId=${fpoAccountId:-fpo.mennat0.testnet}
+
+pair=${pair:-ETH / USD}
+provider=${provider:-provider.mennat0.testnet}
+
+# reset requester account
+NEAR_ENV=$network near delete $accountId $masterAccountId
+NEAR_ENV=$network near create-account $accountId --masterAccount $masterAccountId --initialBalance $initialBalance
+
+NEAR_ENV=$network near call $fpoAccountId get_entry "{\"pair\": \"$pair\", \"provider\": \"$provider\"}" --accountId $accountId --gas=300000000000000
